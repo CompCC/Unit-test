6dbfed861785c9ddfbff887bac629bbf1bcfc94b
Roman <compcc@ya.ru>
2023-11-04 16:45:14 +0300

    add Sem2

---
+++ Lection/Lection1/CalculatorTest2.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.*;
+
+public class CalculatorTest2 {
+    public static void main(String[] args) {
+        // Проверка базового функционала с целыми числами, с использованием утверждений AssertJ:
+        assertThat(Calculator.calculation(2, 6, '+')).isEqualTo(8);
+        assertThat(Calculator.calculation(2, 2, '-')).isEqualTo(0);
+        assertThat(Calculator.calculation(2, 7, '*')).isEqualTo(14);
+        assertThat(Calculator.calculation(100, 50, '/')).isEqualTo(2);
+    }
+}
